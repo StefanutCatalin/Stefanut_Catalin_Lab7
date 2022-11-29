7c9cc5e752ff088a286799facb1be13f76db7b83
StefanutCatalin <stefanut.catalin420@gmail.com>
2022-11-29 15:57:01 +0200

    lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,26 @@
-﻿namespace Stefanut_Catalin_Lab7;
+﻿using System;
+using Stefanut_Catalin_Lab7.Data;
+using System.IO;
+
+namespace Stefanut_Catalin_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+    public App()
 	{
 		InitializeComponent();
 
